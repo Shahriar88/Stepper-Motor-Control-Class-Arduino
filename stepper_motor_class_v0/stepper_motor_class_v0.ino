1ce85f8fe238cf0a3ff66d40de40a000f8f3e30b
Md Shahriar Forhad <89806405+Shahriar88@users.noreply.github.com>
2024-03-10 18:30:31 -0500

    Update stepper_motor_class_v0.ino

--- stepper_motor_class_v0/stepper_motor_class_v0.ino
+++ stepper_motor_class_v0/stepper_motor_class_v0.ino
@@ -1,5 +1,5 @@
 // This object is used to control stepper motor, using stepper motor driver
-// Validated the code with Arduino Mega 2560 + Ramps 1.4 + HR4988 Motor Driver + Nema 17 motor + 24 Volt DC power supply
+// Validated the code with Arduino Mega 2560 + Ramps 1.4 + HR4988 Motor Driver + Nema 17 motor +12 Volt DC power supply
 // Start Copy .................................................................................................
 class stepper_shah{
   public:
